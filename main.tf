cd0bc75dee4bb27af1f6849112d67f6559773a64
Hakikat <harrytechworld@hotmail.com>
2025-10-07 15:33:12 +0200

    add new test environment

--- main.tf
+++ main.tf
@@ -17,58 +17,58 @@ provider "azurerm" {
 variable "vm_names" {
   description = "VM names"
   type        = list(string)
-  default     = ["htw-srv01", "htw-srv02"]
+  default     = ["ptw-srv01", "ptw-srv02"]
 }
 
 # ----------------------------
 # Resource Group
 # ----------------------------
-resource "azurerm_resource_group" "htw_RG" {
-  name     = "htw_RG01"
+resource "azurerm_resource_group" "ptw_RG" {
+  name     = "ptw_RG01"
   location = "West Europe"
 
   tags = {
-    environment = "dev"
-    project     = "htw"
+    environment = "test"
+    project     = "ptw"
   }
 }
 
 # ----------------------------
 # Virtual Network
 # ----------------------------
-resource "azurerm_virtual_network" "htw_vnet" {
-  name                = "htw_vnet01"
-  location            = azurerm_resource_group.htw_RG.location
-  resource_group_name = azurerm_resource_group.htw_RG.name
+resource "azurerm_virtual_network" "ptw_vnet" {
+  name                = "ptw_vnet01"
+  location            = azurerm_resource_group.ptw_RG.location
+  resource_group_name = azurerm_resource_group.ptw_RG.name
   address_space       = ["10.0.0.0/16"]
 
   tags = {
-    environment = "dev"
-    project     = "htw"
+    environment = "test"
+    project     = "ptw"
   }
 }
 
 # ----------------------------
 # Subnet
 # ----------------------------
-resource "azurerm_subnet" "htw_subnet" {
-  name                 = "htw_subnet01"
-  virtual_network_name = azurerm_virtual_network.htw_vnet.name
-  resource_group_name  = azurerm_resource_group.htw_RG.name
+resource "azurerm_subnet" "ptw_subnet" {
+  name                 = "ptw_subnet01"
+  virtual_network_name = azurerm_virtual_network.ptw_vnet.name
+  resource_group_name  = azurerm_resource_group.ptw_RG.name
   address_prefixes     = ["10.0.0.0/24"]
 }
 
 # ----------------------------
 # NSG
 # ----------------------------
-resource "azurerm_network_security_group" "htw_nsg" {
-  name                = "htw_nsg01"
-  location            = azurerm_resource_group.htw_RG.location
-  resource_group_name = azurerm_resource_group.htw_RG.name
+resource "azurerm_network_security_group" "ptw_nsg" {
+  name                = "ptw_nsg01"
+  location            = azurerm_resource_group.ptw_RG.location
+  resource_group_name = azurerm_resource_group.ptw_RG.name
 
   tags = {
-    environment = "dev"
-    project     = "htw"
+    environment = "test"
+    project     = "ptw"
   }
 }
 
@@ -83,68 +83,68 @@ resource "azurerm_network_security_rule" "rdp_rule" {
   destination_port_range      = "3389"
   source_address_prefix       = "*"   # 👈 allows all IPs
   destination_address_prefix  = "*"
-  resource_group_name         = azurerm_resource_group.htw_RG.name
-  network_security_group_name = azurerm_network_security_group.htw_nsg.name
+  resource_group_name         = azurerm_resource_group.ptw_RG.name
+  network_security_group_name = azurerm_network_security_group.ptw_nsg.name
 }
 
 # Associate NSG with Subnet
-resource "azurerm_subnet_network_security_group_association" "htw_SGA" {
-  subnet_id                 = azurerm_subnet.htw_subnet.id
-  network_security_group_id = azurerm_network_security_group.htw_nsg.id
+resource "azurerm_subnet_network_security_group_association" "ptw_SGA" {
+  subnet_id                 = azurerm_subnet.ptw_subnet.id
+  network_security_group_id = azurerm_network_security_group.ptw_nsg.id
 }
 
 # ----------------------------
 # Public IPs
 # ----------------------------
-resource "azurerm_public_ip" "htw_ip" {
+resource "azurerm_public_ip" "ptw_ip" {
   for_each            = toset(var.vm_names)
   name                = "${each.key}-ip"
-  location            = azurerm_resource_group.htw_RG.location
-  resource_group_name = azurerm_resource_group.htw_RG.name
+  location            = azurerm_resource_group.ptw_RG.location
+  resource_group_name = azurerm_resource_group.ptw_RG.name
   allocation_method   = "Static"
   sku                 = "Standard"
   zones               = ["1"]
 
   tags = {
-    environment = "dev"
-    project     = "htw"
+    environment = "test"
+    project     = "ptw"
   }
 }
 
 # ----------------------------
 # NICs
 # ----------------------------
-resource "azurerm_network_interface" "htw_nic" {
+resource "azurerm_network_interface" "ptw_nic" {
   for_each            = toset(var.vm_names)
   name                = "${each.key}-nic"
-  location            = azurerm_resource_group.htw_RG.location
-  resource_group_name = azurerm_resource_group.htw_RG.name
+  location            = azurerm_resource_group.ptw_RG.location
+  resource_group_name = azurerm_resource_group.ptw_RG.name
 
   ip_configuration {
     name                          = "internal"
-    subnet_id                     = azurerm_subnet.htw_subnet.id
+    subnet_id                     = azurerm_subnet.ptw_subnet.id
     private_ip_address_allocation = "Dynamic"
-    public_ip_address_id          = azurerm_public_ip.htw_ip[each.key].id
+    public_ip_address_id          = azurerm_public_ip.ptw_ip[each.key].id
   }
 
   tags = {
-    environment = "dev"
-    project     = "htw"
+    environment = "test"
+    project     = "ptw"
   }
 }
 
 # ----------------------------
 # Windows Server 2022 Virtual Machines
 # ----------------------------
-resource "azurerm_windows_virtual_machine" "htw_vm" {
+resource "azurerm_windows_virtual_machine" "ptw_vm" {
   for_each              = toset(var.vm_names)
   name                  = each.key
-  location              = azurerm_resource_group.htw_RG.location
-  resource_group_name   = azurerm_resource_group.htw_RG.name
+  location              = azurerm_resource_group.ptw_RG.location
+  resource_group_name   = azurerm_resource_group.ptw_RG.name
   size                  = "Standard_D2s_v3"
-  admin_username        = "htwadmin"
+  admin_username        = "ptwadmin"
   admin_password        = "P@ssword12345!"   # ⚠️ hardcoded password
-  network_interface_ids = [azurerm_network_interface.htw_nic[each.key].id]
+  network_interface_ids = [azurerm_network_interface.ptw_nic[each.key].id]
 
   os_disk {
     caching              = "ReadWrite"
@@ -160,8 +160,8 @@ resource "azurerm_windows_virtual_machine" "htw_vm" {
   }
 
   tags = {
-    environment = "dev"
-    project     = "htw"
+    environment = "test"
+    project     = "ptw"
   }
 }
 
@@ -170,5 +170,5 @@ resource "azurerm_windows_virtual_machine" "htw_vm" {
 # ----------------------------
 output "vm_public_ips" {
   description = "Public IPs of the deployed VMs"
-  value       = { for k, ip in azurerm_public_ip.htw_ip : k => ip.ip_address }
+  value       = { for k, ip in azurerm_public_ip.ptw_ip : k => ip.ip_address }
 }
