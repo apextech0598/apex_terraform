5ddd606c7a6553101851947861e4997ee006b862
Hakikat <apextech.no@outlook.com>
2025-10-27 21:37:53 +0100

    final

--- backend.tf
+++ backend.tf
@@ -1,8 +1,8 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "tfstate-rg"
-    storage_account_name = "htwtfstorage001"
-    container_name       = "htwcont"
+    storage_account_name = "apextfstorage001"
+    container_name       = "apexcont"
     key                  = "terraform.tfstate"
   }
 }
